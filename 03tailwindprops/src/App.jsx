27ef371d423c88cbdb1930f177a28210adf1fafa
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-03-17 17:57:27 +0530

    Basic Tailwind css completed

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -10,7 +10,9 @@ function App() {
   return (
     <>
    <h1 className='text-3xl bg-green-500'>Vite with Tailwind</h1>
-   <Card></Card>
+   <Card source="https://img.freepik.com/free-photo/sports-car-driving-asphalt-road-night-generative-ai_188544-8052.jpg?w=900&t=st=1710660574~exp=1710661174~hmac=5a25d79b570876c96539a264e4d17734eab43e3724e59de2a0387582a3da1086"></Card>
+   <Card username="Hello"></Card>
+   <Card username=""></Card>
     </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Card() {
+function Card({source="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?w=900&t=st=1710660452~exp=1710661052~hmac=0dfd5994e2dc27873d57d6205d16df4a44bbb9b9e2b387cdfd8720825b379699",username="Car"}) {
   return (
     <div>
-    <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img class="w-24 h-24 rounded-full mx-auto" src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?w=900&t=st=1710612421~exp=1710613021~hmac=68326f3b5cb38fe42cb141141d2975cce516bdc1602bee8fb51b69bc8c7ee3cd" alt="" width="384" height="512"/>
-  <div class="pt-6 space-y-4">
+    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={source} alt="" width="384" height="512"/>
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p class="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
@@ -13,12 +13,12 @@ function Card() {
         and the build size is tiny.”
       </p>
     </blockquote>
-    <figcaption class="font">
-      <div>
-        Sarah Dayan
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400" >
+      {username} 
       </div>
-      <div>
-        Staff Engineer, Algolia
+      <div class="text-slate-700 dark:text-slate-500">
+        Beauty
       </div>
     </figcaption>
   </div>
