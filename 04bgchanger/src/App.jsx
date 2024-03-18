b3983a4582d7f9d39a54c8fec2ba2f75a5a0204a
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-03-18 18:41:41 +0530

    BG-changer

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -5,15 +5,22 @@ import './App.css'
 
 function App() {
   const [color, setColor] = useState('olive')
-
+  //  function changeColor(color){
+  //   setColor(color)
+  //  }
   return (
     <>
-     {/* <h1 className='bg-green-500 text-3xl'>Vite BG changer</h1> */}
-     <div className='w-full h-screen duration-200 bg-black' style={{backgroundColor:color}} ></div>
-     <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
-      onClick={()=>changeColor('red')}>Test</button>
-      <button className='outline-none px-4 py-1 rounded-full text-green-600 shadow-lg'
-      >Test</button>
+      <div className=" grid ">
+
+        <div className=" min-h-[550px] " style={{backgroundColor:color}}></div>
+        <div className=" min-h-[50px] grid bg-cyan-800 gap-2 grid-cols-4">
+          <button className="bg-yellow-300 rounded-xl" onClick={()=>{setColor("yellow")}}>Yellow</button>
+          <button className="bg-lime-700 rounded-xl" onClick={()=>{setColor("green")}}>Green</button>
+          <button className="bg-red-600 rounded-xl" onClick={()=>{setColor("red")}}>Red</button>
+          <button className="bg-blue-500 rounded-xl"onClick={()=>{setColor("blue")}}>Blue</button>
+        </div>
+
+      </div>
     </>
   )
 }
