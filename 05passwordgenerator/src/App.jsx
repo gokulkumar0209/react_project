098eb51ccadbd01ef4fcdafa761bcb1300f4ef08
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-03-20 20:55:32 +0530

    Completed Password Generator

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback,useEffect } from 'react'
+import { useState, useCallback,useEffect,useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,7 +9,7 @@ function App() {
   const [symbolAllowed,setSymbolAllowed]=useState(false)
   const [password,setPassword]=useState('')
 
-  
+  const passwordRef=useRef()
   const generatePassword=useCallback(()=>{
      let pass=""
      let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -25,14 +25,18 @@ function App() {
      }
      setPassword(pass)
   },[length,numberAllowed,symbolAllowed])
+  const copyPasswordToClipBoard=()=>{
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select()
+  }
 useEffect(()=>{generatePassword()},[length,numberAllowed,symbolAllowed])
   return (
     <>
-    <div>
-      <h1>Password Generator</h1>
+    <div className='grid justify-center items-center center h-40 m-40 bg-gray-300'>
+      <h1 className=' text-black'>Password Generator</h1>
       <div>
-          <input type='text' value={password} readOnly placeholder='Password'></input>
-          <button>Copy</button>
+          <input type='text' value={password} readOnly placeholder='Password' ref={passwordRef}></input>
+          <button className="  bg-blue-400 rounded-sm"   onClick={()=>{copyPasswordToClipBoard()}}>Copy</button>
       </div>
       <div>
           <input type='range' min={8} max={20} value={length} onChange={(e)=>{setLength(e.target.value)}}/> 
