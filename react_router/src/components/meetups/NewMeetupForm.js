71ebd053b014f2b1a3d6f02bedeb2210b6ecd01f
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-04-05 20:19:38 +0530

    Created form to add Items

--- react_router/src/components/meetups/NewMeetupForm.js
+++ react_router/src/components/meetups/NewMeetupForm.js
@@ -1,14 +1,65 @@
-import React from 'react'
-
+import React from "react";
+import { useRef } from "react";
 function NewMeetupForm() {
-  return (
-    <>
-    <h1>Add Meetup</h1>
-    <form>
-        
-    </form>
-    </>
-  )
+  function sumbitHandler(event){
+    event.preventDefault()
+   console.log("works")
+  }
+	return (
+		<div className="flex justify-center">
+			<div className="w-96">
+				<h1 className="mb-2 font-serif font-bold text-xl flex justify-center">Add Meetup</h1>
+
+				<form onSubmit={sumbitHandler}>
+					<div className="bg-gray-400 p-4 rounded-md">
+						<div className="mb-2">
+							<label htmlFor="title">Meetup Title</label>
+							<input
+								type="text"
+								required
+								id="title"
+								className="w-full border rounded-md px-4 py-2"
+							/>
+						</div>
+						<div className="mb-2">
+							<label htmlFor="image">Meetup Image</label>
+							<input
+								type="url"
+								required
+								id="image"
+								className="w-full border rounded-md px-4 py-2"
+							/>
+						</div>
+						<div className="mb-2">
+							<label htmlFor="address">Address</label>
+							<input
+								type="text"
+								required
+								id="address"
+								className="w-full border rounded-md px-4 py-2"
+							/>
+						</div>
+						<div className="mb-2">
+							<label htmlFor="description">Description</label>
+							<textarea
+								required
+								rows="5"
+								className="w-full border rounded-md px-4 py-2"
+							></textarea>
+						</div>
+						<div className="flex justify-end">
+							<button
+								type="submit"
+								className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+							 >
+								Add Meetup
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
-export default NewMeetupForm
+export default NewMeetupForm;
--- react_router/src/pages/NewMeetups.js
+++ react_router/src/pages/NewMeetups.js
@@ -1,10 +1,11 @@
 import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
 	return (
 		<>
 			<div className=" bg-neutral-300 h-screen flex justify-center">
-				Add Meetup
+				<NewMeetupForm></NewMeetupForm>
 			</div>
 		</>
 	);
