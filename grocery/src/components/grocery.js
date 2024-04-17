9843a49b351879e8e8f0d4af85729096504dc56f
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-04-17 23:59:08 +0530

    Creating Inputfield, need to correct the issue

--- grocery/src/components/grocery.js
+++ grocery/src/components/grocery.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { useState } from "react";
 
-const [gros,setGros]=useState([])
+const [gros, setGros] = useState([]);
 function grocery() {
-	return <div>grocery</div>;
+	return (
+		<>
+			<div>grocery</div>
+			<input type="textbox"></input>
+			<button>ADD</button>
+		</>
+	);
 }
 
 export default grocery;
