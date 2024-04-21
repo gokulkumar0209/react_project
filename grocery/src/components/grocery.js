f89db1d2f47fbb13a0ffff7521e096b44db29350
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-04-21 19:03:34 +0530

    To-Do list done

--- grocery/src/components/grocery.js
+++ grocery/src/components/grocery.js
@@ -1,44 +1,81 @@
 import React, { useState } from "react";
 
 function Grocery() {
-	const [groceries, setGroceries] = useState([]);
+	const [groceries, setGroceries] = useState({});
 	const [inputValue, setInputValue] = useState("");
+	const [quantity, setQuantity] = useState(1);
+	const [error, setError] = useState("");
 
 	const handleInputChange = (e) => {
 		setInputValue(e.target.value);
+		setError("");
 	};
-
+	const handleQuantity = (e) => {
+		setQuantity(e.target.value);
+	};
 	const handleAddClick = () => {
-		if (inputValue.trim() !== "") {
-			setGroceries([...groceries, inputValue]);
-			setInputValue("");
+		const trimmedValue = inputValue.trim();
+
+		if (trimmedValue !== "") {
+			if (!groceries[trimmedValue]) {
+				setGroceries({
+					...groceries,
+					[trimmedValue]: { name: trimmedValue, quantity: quantity },
+				});
+				setInputValue("");
+			} else {
+				setError("Item already exists");
+			}
+		} else {
+			setError("Please enter a valid item");
 		}
+		setQuantity(0);
 	};
 
-	const handleDeleteClick = (index) => {
-		const updatedGroceries = [...groceries];
-		updatedGroceries.splice(index, 1);
+	const handleDeleteClick = (name) => {
+		const updatedGroceries = { ...groceries };
+		delete updatedGroceries[name];
 		setGroceries(updatedGroceries);
 	};
 
 	return (
 		<>
-			<div>My Grocery List</div>
-			<input
-				type="text"
-				value={inputValue}
-				onChange={handleInputChange}
-				placeholder="Enter grocery item"
-			/>
-			<button onClick={handleAddClick}>ADD</button>
-			<ul>
-				{groceries.map((item, index) => (
-					<li key={index}>
-						{item}
-						<button onClick={() => handleDeleteClick(index)}>Delete</button>
-					</li>
-				))}
-			</ul>
+			<div>
+				<div className="flex justify-center">
+					<input
+						className=" bg-slate-300 border-zinc-600 mr-2 mb-2"
+						type="text"
+						value={inputValue}
+						onChange={handleInputChange}
+						placeholder="Enter grocery item"
+					/>
+
+					<input
+						className=" bg-slate-300 border-zinc-600  w-9 mr-2 mb-2"
+						type="number"
+						value={quantity}
+						onChange={handleQuantity}
+					/>
+
+					<button onClick={handleAddClick}>ADD</button>
+				</div>
+				<div className="flex justify-center">
+					{error && <p>{error}</p>}
+					<ul>
+						{Object.keys(groceries).map((name) => (
+							<li className=" mb-2" key={name}>
+								{groceries[name].name}
+								<button
+									className=" ml-4"
+									onClick={() => handleDeleteClick(name)}
+								>
+									Delete
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</>
 	);
 }
