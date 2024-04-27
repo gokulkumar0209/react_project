73d4cd4e5744634be8e69cfc98edc1fd93c0efa2
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-04-27 22:44:43 +0530

    Created a empty file to create newmovieform

---
+++ reel-list/src/movies/NewMoviesForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useRef } from 'react'
+function NewMoviesForm() {
+    const titleInputRef=useRef()
+    const imagInputRef=useRef()
+   
+  return (
+    <div>NewMoviesForm</div>
+  )
+}
+
+export default NewMoviesForm
