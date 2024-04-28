93334308859db5c8e79f8a6321cde35df8649903
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-04-28 20:45:09 +0530

    "added favorite button"

--- reel-list/src/components/movies/Movieitem.js
+++ reel-list/src/components/movies/Movieitem.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Movieitem( {title, image, description}) {
+function Movieitem({ title, image, description }) {
 	return (
 		<li>
 			<div>
 				<h2>{title}</h2>
 				<img src={image}></img>
 				<p>{description}</p>
+				<button>Add</button>
 			</div>
 		</li>
 	);
